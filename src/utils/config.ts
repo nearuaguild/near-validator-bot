9a895e4f83922b245ec00ce5ab95c305ecb8cf71
hlib <kiskesis@gmail.com>
2023-01-27 14:45:43 +0100

    * remove accoundid from .env and config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,20 +6,18 @@ interface ConfigFields {
   botToken: string,
   channelId: string,
   poolId: string,
-  accountId: string,
 }
 
 const config = validateConfig();
 
 function validateConfig(): ConfigFields {
-  if (!process.env["BOT_TOKEN"] || !process.env["CHANNEL_ID"] || !process.env["POOL_ID"] || !process.env["ACCOUNT_ID"]) {
+  if (!process.env["BOT_TOKEN"] || !process.env["CHANNEL_ID"] || !process.env["POOL_ID"]) {
     throw Error("Invalid .env file, please check fields");
   } else {
     return {
       botToken: process.env["BOT_TOKEN"],
       channelId: process.env["CHANNEL_ID"],
       poolId: process.env["POOL_ID"],
-      accountId: process.env["ACCOUNT_ID"]
     }
   }
 }
